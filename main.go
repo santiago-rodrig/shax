f12dde282ac8449453c8402621beb30be537e2da
santiago-rodrig <santo1996.29@gmail.com>
2021-03-06 00:08:34 -0400

    add implementation with 384 and 512

--- main.go
+++ main.go
@@ -3,13 +3,35 @@ package main
 import (
 	"bufio"
 	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 )
 
+var algorithms = [3]uint{256, 384, 512}
+
+func algorithmIsValid(algorithm uint) bool {
+	for _, a := range algorithms {
+		if algorithm == a {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
+	algorithm := flag.Uint("algo", 256, "-algo=256")
+	flag.Parse()
+
+	if !algorithmIsValid(*algorithm) {
+		fmt.Printf("Invalid algorithm: %d\nValid values are: 256, 384, and 512\n\n", *algorithm)
+		flag.PrintDefaults()
+	}
+
 	buff := bufio.NewReader(os.Stdin)
 	bs, err := buff.ReadBytes('\n')
 
@@ -19,6 +41,15 @@ func main() {
 		}
 	}
 
-	c1 := sha256.Sum256(bs)
-	fmt.Printf("%x\n", c1)
+	switch *algorithm {
+	case 256:
+		c1 := sha256.Sum256(bs)
+		fmt.Printf("%x\n", c1)
+	case 384:
+		c1 := sha512.Sum384(bs)
+		fmt.Printf("%x\n", c1)
+	case 512:
+		c1 := sha512.Sum512(bs)
+		fmt.Printf("%x\n", c1)
+	}
 }
