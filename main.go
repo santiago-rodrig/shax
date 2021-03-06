ede4aea36fe8fa429c83f2c8e3594bf12e2b9baa
santiago-rodrig <santo1996.29@gmail.com>
2021-03-05 23:51:38 -0400

    first implementation with sha256

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"bufio"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
-	println("not implemented")
+	buff := bufio.NewReader(os.Stdin)
+	bs, err := buff.ReadBytes('\n')
+
+	if err != nil {
+		if err != io.EOF {
+			log.Fatal(err)
+		}
+	}
+
+	c1 := sha256.Sum256(bs)
+	fmt.Printf("%x\n", c1)
 }
